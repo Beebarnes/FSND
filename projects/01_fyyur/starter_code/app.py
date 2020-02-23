c1d942b95d91bc40dfa35ae36dca41c9d7d8061c
Brian Barnes <beebarnes3@gmail.com>
2020-02-23 13:49:57 -0500

    update venue and artist columns

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -8,6 +8,7 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -20,8 +21,10 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
+# COMPLETED IN CONFIG.PY
 
 #----------------------------------------------------------------------------#
 # Models.
@@ -32,28 +35,38 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.String())
+    address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
-
+    seeking_talen = db.Column(db.Boolean, default=True)
+    seeking_description= db.Column(db.String(540))
+    image_link = db.Column(db.String(500))
+    past_shows = db.Column(db.String())
+    upcoming_shows = db.Column(db.String())
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.String)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
-
+    seeking_venue = db.Column(db.Boolean, default=True)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+    past_shows = db.Column(db.String())
+    upcoming_shows = db.Column(db.String())    
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
@@ -219,6 +232,7 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
+
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
 
